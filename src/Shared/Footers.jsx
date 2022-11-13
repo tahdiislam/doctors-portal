e1e74adf4741593c6969ebe3d38948be8f60b6e1
tahdi-islam <tshdi60@gmail.com>
2022-11-13 08:25:43 +0600

    create footer

--- src/Shared/Footers.jsx
+++ src/Shared/Footers.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footers = () => {
     return (
-        <div>
-            This is footers
-        </div>
+        <footer className="p-10">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Branding</Link>
+                    <Link to="/" className="link link-hover">Design</Link>
+                    <Link to="/" className="link link-hover">Marketing</Link>
+                    <Link to="/" className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Jobs</Link>
+                    <Link to="/" className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="/" className="link link-hover">Terms of use</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                </div>
+            </div>
+            <div className="footer-center mt-10">
+                <div>
+                    <p>Copyright 2022 All Rights Reserved</p>
+                </div>
+            </div>
+        </footer>
     );
 };
 
