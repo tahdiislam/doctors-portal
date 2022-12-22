74ab0833e2add3874a84ff31477e882dd5d65fc4
tahdi-islam <tshdi60@gmail.com>
2022-12-22 08:49:24 +0600

    add loader in the main page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footers from '../Shared/Footers';
-import Headers from '../Shared/Headers';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Footers from "../Shared/Footers";
+import Headers from "../Shared/Headers";
+import GridLoader from "react-spinners/GridLoader";
 
 const Main = () => {
-    return (
-        <div>
-          <Headers/>
-          <Outlet/>
-          <Footers/>  
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  return (
+    <section>
+      {loading ? (
+        <div className="h-screen w-full flex justify-center items-center">
+          <GridLoader
+            color={"#10CEE7"}
+            loading={loading}
+            size={15}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
         </div>
-    );
+      ) : (
+        <div>
+          <Headers />
+          <Outlet />
+          <Footers />
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default Main;
